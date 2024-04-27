e738276dd29139dd0f98a373136185bc3a2bb66d
a-r3n <a-r3n@users.noreply.github.com>
2024-04-27 11:33:31 +1000

    Updated server.js to resolve Apollo error

--- backend/server.js
+++ backend/server.js
@@ -6,40 +6,40 @@ const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 4000;
 
-// MongoDB URI from environment variable
+const port = process.env.PORT || 4000;
 const uri = process.env.MONGODB_URI;
 
-// Start server function
 async function startServer() {
     const server = new ApolloServer({ typeDefs, resolvers });
-
-    // Starting the Apollo server
     await server.start();
-    
-    // Apply GraphQL middleware
     server.applyMiddleware({ app });
 
-    // Connect to MongoDB
     await mongoose.connect(uri, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 5000
+        useUnifiedTopology: true
     });
-    
+
     console.log("MongoDB connected successfully.");
 
-    // Start Express server
+    // Serve static files from the React app
+    app.use(express.static(path.join(__dirname, 'build')));
+
+    // The "catchall" handler: for any request that doesn't
+    // match one above, send back React's index.html file.
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+
     app.listen(port, () => {
         console.log(`Server running on port ${port}`);
     });
 }
 
-// Catching potential errors
 startServer().catch(error => {
     console.error("Failed to connect to MongoDB", error);
     process.exit(1);
